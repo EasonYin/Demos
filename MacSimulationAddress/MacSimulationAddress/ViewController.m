d46a6a0f705c294ba8c7abffe4538f77254b6f4a
yinhuadong <yinhuadong@jd.com>
2019-04-30 10:27:41 +0800

    no message

--- MacSimulationAddress/MacSimulationAddress/ViewController.m
+++ MacSimulationAddress/MacSimulationAddress/ViewController.m
@@ -10,9 +10,9 @@
 #import <MapKit/MapKit.h>
 #import "EYLocationConverter.h"
 
-@interface ViewController ()<MKMapViewDelegate,NSMenuDelegate>
+@interface ViewController ()<MKMapViewDelegate>
 @property (weak) IBOutlet MKMapView *mapView;
-
+@property (nonatomic,assign) CLLocationCoordinate2D coordinate;
 @end
 
 @implementation ViewController
@@ -22,8 +22,6 @@
     
     // Do any additional setup after loading the view.
     
-    self.mapView.showsUserLocation = YES;
-
 }
 
 #pragma mark - MKMapViewDelegate
@@ -40,22 +38,52 @@
 // 3、显示大头针时触发，返回大头针视图，通常自定义大头针可以通过此方法进行
 - (nullable MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation{
     
+    if ([annotation isKindOfClass:[MKPointAnnotation class]]) {
+        // Try to dequeue an existing pin view first.（这里跟UITableView的重用差不多）
+        MKPinAnnotationView *customPinView = (MKPinAnnotationView*)[mapView
+                                                                    dequeueReusableAnnotationViewWithIdentifier:@"CustomPinAnnotationView"];
+        if (!customPinView){
+            // If an existing pin view was not available, create one.
+            customPinView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation
+                                                            reuseIdentifier:@"CustomPinAnnotationView"];
+        }
+        //iOS9中用pinTintColor代替了pinColor
+        customPinView.animatesDrop = YES;
+        customPinView.canShowCallout = YES;
+        
+        //左边
+        customPinView.leftCalloutAccessoryView = nil;
+
+        //右边
+//        NSButton *btn = [NSButton buttonWithTitle:@"复制坐标" target:self action:@selector(copyAnnotation:)];
+//        customPinView.rightCalloutAccessoryView = btn;
+        customPinView.rightCalloutAccessoryView = nil;
+
+        NSTextView *text = [[NSTextView alloc]initWithFrame:CGRectMake(0, 0, 200, 40)];
+        text.backgroundColor = [NSColor clearColor];
+        text.string = [NSString stringWithFormat:@"lat=\"%.6f\" lon=\"%.6f\"",self.coordinate.latitude,self.coordinate.longitude];;
+        customPinView.detailCalloutAccessoryView = text;
+        
+        return customPinView;
+    }
+    
     return nil;
 }
 
 // 4、点击选中某个大头针时触发
 - (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view{
     
-    CLLocationCoordinate2D wgs84 = [EYLocationConverter gcj02ToWgs84:view.annotation.coordinate];
     
-    NSAlert *alert = [[NSAlert alloc]init];
-    alert.messageText = [NSString stringWithFormat:@"lat=\"%.6f\" lon=\"%.6f\"",wgs84.latitude,wgs84.longitude];
-    [alert addButtonWithTitle:@"确定"];
-    
-    NSWindowController *window = [[NSStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]] instantiateInitialController];
-    [alert beginSheetModalForWindow:window.window completionHandler:^(NSModalResponse returnCode) {
-        
-    }];
+//    CLLocationCoordinate2D wgs84 = [EYLocationConverter gcj02ToWgs84:view.annotation.coordinate];
+//
+//    NSAlert *alert = [[NSAlert alloc]init];
+//    alert.messageText = [NSString stringWithFormat:@"lat=\"%.6f\" lon=\"%.6f\"",wgs84.latitude,wgs84.longitude];
+//    [alert addButtonWithTitle:@"确定"];
+//
+//    NSWindowController *window = [[NSStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]] instantiateInitialController];
+//    [alert beginSheetModalForWindow:window.window completionHandler:^(NSModalResponse returnCode) {
+//
+//    }];
 
 }
 
@@ -77,23 +105,32 @@
     NSLog(@"%@",NSStringFromPoint(event.locationInWindow));
     CLLocationCoordinate2D coordinate = [self.mapView convertPoint:event.locationInWindow toCoordinateFromView:self.view];
     NSLog(@"%f==%f",coordinate.latitude,coordinate.longitude);
+    self.coordinate = coordinate;
+    
+    NSMenu *menu = [[NSMenu alloc]initWithTitle:@"Menu"];
+    NSMenuItem * item1 = [[NSMenuItem alloc]initWithTitle:@"添加大头钉" action:@selector(addAnnotationToMapView:) keyEquivalent:@""];
+    item1.target = self;
+    [menu addItem:item1];
+    [NSMenu popUpContextMenu:menu withEvent:event forView:self.view];
+}
+
+- (void)addAnnotationToMapView:(NSMenuItem *)sender{
     
     MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
     // 配置标注数据源
-    annotation.coordinate = coordinate;
+    annotation.coordinate = self.coordinate;
     annotation.title = @"标注位置";
-    annotation.subtitle = [NSString stringWithFormat:@"经度：%.6f, 纬度：%.6f", coordinate.longitude, coordinate.latitude];
+    annotation.subtitle = [NSString stringWithFormat:@"经度：%.6f, 纬度：%.6f", self.coordinate.longitude, self.coordinate.latitude];
     // 在地图上添加标注
     [self.mapView addAnnotation:annotation];
     
-//    NSMenu * popupMenu = [[NSMenu alloc] initWithTitle:@"test"];
-//    popupMenu.delegate = self;
-//
-//    NSMenuItem *item = [[NSMenuItem separatorItem] initWithTitle:@"添加大头钉" action:@selector(addAnnotationToMap:) keyEquivalent:@"111"];
-//    [item setTarget:self];
-//    [popupMenu addItem:item];
-//    [NSMenu popUpContextMenu:popupMenu withEvent:event forView:self.mapView];
 }
 
+//- (void)copyAnnotation:(id)sender{
+//
+//    NSString *copyString = [NSString stringWithFormat:@"lat=\"%.6f\" lon=\"%.6f\"",self.coordinate.latitude,self.coordinate.longitude];
+//    [[NSPasteboard pasteboardWithUniqueName] setString:copyString forType:NSPasteboardTypeString];
+//
+//}
 
 @end
