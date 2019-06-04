138da5105218576f82c8fb9e09290e8641492e19
yinhuadong <yinhuadong@jd.com>
2019-06-04 11:13:22 +0800

    RN项目解耦

--- RNDemo/RNDemo/RNDemo/RNDemoViewController.m
+++ RNDemo/RNDemo/RNDemo/RNDemoViewController.m
@@ -30,15 +30,15 @@
     
 }
 
-//- (void)viewWillAppear:(BOOL)animated{
-//    [super viewWillAppear:animated];
-//    [self.navigationController setNavigationBarHidden:YES animated:YES];
-//}
-//
-//- (void)viewWillDisappear:(BOOL)animated{
-//    [super viewWillDisappear:animated];
-//    [self.navigationController setNavigationBarHidden:NO animated:YES];
-//
-//}
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+}
+
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+
+}
 
 @end
