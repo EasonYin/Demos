0be67a46155826f79035ec1d034ddec1f25388a5
yinhuadong <yinhuadong@jd.com>
2019-08-14 18:03:23 +0800

    [update]

--- EYQRCodeTools/EYQRCodeTools/Classes/EYQRCodeScanViewController.m
+++ EYQRCodeTools/EYQRCodeTools/Classes/EYQRCodeScanViewController.m
@@ -68,29 +68,16 @@
     [super viewWillAppear:animated];
 
     [self.navigationController setNavigationBarHidden:YES animated:NO];
-//    //设置导航栏背景图片为一个空的image，这样就透明了
-//    [self.navigationController.navigationBar setBackgroundImage:[[UIImage alloc] init] forBarMetrics:UIBarMetricsDefault];
-//
-//    //去掉透明后导航栏下边的黑边
-//    [self.navigationController.navigationBar setShadowImage:[[UIImage alloc] init]];
-
+    
     [self startScanning];
 }
 
-- (void)viewDidDisappear:(BOOL)animated{
-    [super viewDidDisappear:animated];
+- (void)viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+
     [self.navigationController setNavigationBarHidden:NO animated:NO];
 
 }
-
-//- (void)viewWillDisappear:(BOOL)animated{
-//    [super viewWillDisappear:animated];
-//
-//    [self.navigationController setNavigationBarHidden:NO animated:NO];
-////    //    如果不想让其他页面的导航栏变为透明 需要重置
-////    [self.navigationController.navigationBar setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
-////    [self.navigationController.navigationBar setShadowImage:nil];
-//}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -140,6 +127,11 @@
     
     [self.navigationController popViewControllerAnimated:YES];
     
+}
+
+- (void)dealloc {
+    
+    NSLog(@"%s", __func__);
 }
 
 #pragma mark - Action functions
